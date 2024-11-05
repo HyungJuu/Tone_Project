409ed1f705db0d0b4c4b2b4160a11f91398fee14
HyungJuu <rmsdk617@naver.com>
2024-11-05 12:10:52 +0900

    주석제거

--- ToneProject/LoginApp/Utils/ValidationHelper.cs
+++ ToneProject/LoginApp/Utils/ValidationHelper.cs
@@ -3,19 +3,9 @@ using System.Text.RegularExpressions;
 
 namespace LoginApp.Utils
 {
-    /// <summary>
-    /// 로그인 및 회원가입과 관련된 입력값 검증 클래스
-    /// </summary>
     public static partial class ValidationHelper
     {
         #region 로그인 관련 검증
-        /// <summary>
-        /// 로그인 시 입력값 검증
-        /// </summary>
-        /// <param name="id">아이디</param>
-        /// <param name="password">비밀번호</param>
-        /// <param name="dbContext"></param>
-        /// <returns>결과 메시지 및 입력값 초기화 여부</returns>
         public static (bool IsValid, string Message, bool ClearId, bool ClearPassword) ValidateSignInput(string id, string password, UserInfoContext dbContext)
         {
             if (string.IsNullOrEmpty(id))
@@ -37,76 +27,47 @@ namespace LoginApp.Utils
 
         #region 회원가입(계정) 관련 검증
 
-        /// <summary>
-        /// .NET 7.0 기능.
-        /// 정규식을 미리 컴파일하여 사용하도록 정의.
-        /// 성능 향상, 코드 관리
-        /// </summary>
         [GeneratedRegex(@"^[a-zA-Z0-9]*$")]
         private static partial Regex IdRegex();
 
-        /// <summary>
-        /// 회원가입 시 아이디 입력값 검증
-        /// </summary>
-        /// <param name="userId">사용자 입력 아이디</param>
-        /// <param name="context"></param>
-        /// <returns>오류 메시지</returns>
         public static string ValidateUserId(string userId, UserInfoContext context)
         {
             int letterCount = userId.Count(char.IsLetter);
 
             if (string.IsNullOrEmpty(userId))
-            {
                 return "아이디를 입력해주세요.";
-            }
+
             if (!IdRegex().IsMatch(userId))
-            {
                 return "아이디는 영문과 숫자만 입력 가능합니다.";
-            }
+
             if (letterCount < 4)
-            {
                 return "영문자를 4자리 이상 입력해주세요.";
-            }
+
             if (CheckUserIdExist(userId, context))
-            {
                 return "이미 존재하는 아이디입니다.";
-            }
+
             return string.Empty;
         }
 
         [GeneratedRegex(@"^[a-zA-Z](?=.*\d)(?=.*[!@#$%]).+$")]
         private static partial Regex PasswordRegex();
 
-        /// <summary>
-        /// 회원가입 시 비밀번호 입력값 검증
-        /// </summary>
-        /// <param name="password">사용자 입력 비밀번호</param>
-        /// <returns>오류 메시지</returns>
         public static string ValidatePassword(string password)
         {
             int letterCount = password.Count(char.IsLetter);
 
             if (string.IsNullOrEmpty(password))
-            {
                 return "비밀번호를 입력해주세요.";
-            }
+
             if (letterCount < 6)
-            {
                 return "영문자를 6자리 이상 입력해주세요.";
-            }
+
             if (!PasswordRegex().IsMatch(password))
-            {
                 return "숫자, 특수문자(!,@,#,$,%)를 포함해야 합니다.";
-            }
+            
             return string.Empty;
         }
 
-        /// <summary>
-        /// 비밀번호 확인 입력값 검증
-        /// </summary>
-        /// <param name="password">사용자 입력 비밀번호</param>
-        /// <param name="confirmPassword">비밀번호 재입력값</param>
-        /// <returns>오류 메시지</returns>
         public static string ValidateConfirmPassword(string password, string confirmPassword)
         {
             if (string.IsNullOrEmpty(confirmPassword))
@@ -120,22 +81,12 @@ namespace LoginApp.Utils
             return string.Empty;
         }
 
-        /// <summary>
-        /// 데이터베이스에 아이디 존재유무 확인
-        /// </summary>
+        
         private static bool CheckUserIdExist(string userId, UserInfoContext context)
         {
             return context.UserInfos.Any(u => u.Id == userId);
         }
 
-        /// <summary>
-        /// 회원가입 계정 검증
-        /// </summary>
-        /// <param name="userId">아이디</param>
-        /// <param name="password">비밀번호</param>
-        /// <param name="confirmPassword">비밀번호 확인</param>
-        /// <param name="context">데이터베이스 컨텍스트</param>
-        /// <returns>유효성 상태 및 각 상태메시지 반환</returns>
         public static (bool isValid, string IdStatus, string PasswordStatus, string ConfirmPasswordStatus) ValidateSignUpInput(
             string userId, string password, string confirmPassword, UserInfoContext context)
         {
@@ -150,11 +101,7 @@ namespace LoginApp.Utils
         #endregion
 
         #region 회원가입(개인정보) 관련 검증
-        /// <summary>
-        /// 이름 입력값 검증
-        /// </summary>
-        /// <param name="name">이름</param>
-        /// <returns>오류메시지 및 빈문자열 반환</returns>
+        
         public static string ValidateName(string name)
         {
             if (string.IsNullOrEmpty(name))
@@ -164,11 +111,7 @@ namespace LoginApp.Utils
             return string.Empty;
         }
 
-        /// <summary>
-        /// 생년월일 입력값 검증
-        /// </summary>
-        /// <param name="birth">생년월일</param>
-        /// <returns>오류메시지 및 빈문자열 반환</returns>
+        
         public static string ValidateBirth(string birth)
         {
             if (string.IsNullOrEmpty(birth))
@@ -182,12 +125,6 @@ namespace LoginApp.Utils
             return string.Empty;
         }
 
-        /// <summary>
-        /// 회원가입 사용자정보 검증
-        /// </summary>
-        /// <param name="name">이름</param>
-        /// <param name="birth">생년월일</param>
-        /// <returns>유효성 상태를 나타내는 튜플을 반환</returns>
         public static (bool isValid, string nameStatus, string birthStatus) ValidateSignUpInput(
             string name, string birth)
         {
