187922b638c566333544bbd78b4a84327d31a93f
HyungJuu <rmsdk617@naver.com>
2024-11-06 10:50:16 +0900

    Update ValidationHelper.cs

--- ToneProject/LoginApp/Utils/ValidationHelper.cs
+++ ToneProject/LoginApp/Utils/ValidationHelper.cs
@@ -26,6 +26,23 @@ namespace LoginApp.Utils
         }
 
         /// <summary>
+        /// 로그인 성공 시 반환되는 결과
+        /// </summary>
+        public static readonly SignInResult SignInSuccess = new(true, string.Empty, false, false);
+        /// <summary>
+        /// 아이디 미입력 시 반환되는 결과
+        /// </summary>
+        public static readonly SignInResult EmptyUserId = new(false, "아이디를 입력하세요", true, false);
+        /// <summary>
+        /// 비밀번호 미입력 시 반환되는 결과
+        /// </summary>
+        public static readonly SignInResult EmptyUserPwd = new(false, "비밀번호를 입력하세요", false, true);
+        /// <summary>
+        /// 아이디 또는 비밀번호가 일치하지 않을 시 반환되는 결과
+        /// </summary>
+        public static readonly SignInResult IncorrectIdOrPassword = new(false, "아이디 또는 비밀번호가 올바르지 않습니다", true, true);
+
+        /// <summary>
         /// 로그인 입력 확인 메서드
         /// </summary>
         /// <param name="id">사용자 아이디</param>
@@ -34,17 +51,23 @@ namespace LoginApp.Utils
         public static SignInResult CheckSignIn(string id, string password)
         {
             if (string.IsNullOrEmpty(id))
-                return new SignInResult(false, "아이디를 입력하세요", true, false);
+            {
+                return EmptyUserId;
+            }
 
             if (string.IsNullOrEmpty(password))
-                return new SignInResult(false, "비밀번호를 입력하세요", false, true);
+            {
+                return EmptyUserPwd;
+            }
 
             UserInfo? user = _dbContext.UserInfos.SingleOrDefault(u => u.Id == id);
 
             if (user == null || user.Pwd != password)
-                return new SignInResult(false, "아이디 또는 비밀번호가 올바르지 않습니다", true, true);
+            {
+                return IncorrectIdOrPassword;
+            }
 
-            return new SignInResult(true, string.Empty, false, false);
+            return SignInSuccess;
         }
         #endregion
 
@@ -66,6 +89,11 @@ namespace LoginApp.Utils
         }
 
         /// <summary>
+        /// 회원가입(계정) 성공 시 반환되는 결과
+        /// </summary>
+        public static readonly SignUpAccountResult SignUpAccountSuccess = new(true, string.Empty, string.Empty, string.Empty);
+
+        /// <summary>
         /// 아이디 입력 정규식
         /// </summary>
         [GeneratedRegex(@"^[a-zA-Z0-9]*$")]
@@ -79,20 +107,30 @@ namespace LoginApp.Utils
         private static string CheckId(string userId)
         {
             int letterCount = userId.Count(char.IsLetter);
+            string inputIdResult = string.Empty;
 
             if (string.IsNullOrEmpty(userId))
-                return "아이디를 입력해주세요.";
+            {
+                inputIdResult = "아이디를 입력해주세요.";
+            }
+            else if (!IdRegex().IsMatch(userId))
+            {
+                inputIdResult = "아이디는 영문과 숫자만 입력 가능합니다.";
+            }
+            else if (letterCount < 4)
+            {
+                inputIdResult = "영문자를 4자리 이상 입력해주세요.";
+            }
+            else if (_dbContext.UserInfos.Any(u => u.Id == userId))
+            {
+                inputIdResult = "이미 존재하는 아이디입니다.";
+            }
+            else
+            {
+                return inputIdResult;
+            }
 
-            if (!IdRegex().IsMatch(userId))
-                return "아이디는 영문과 숫자만 입력 가능합니다.";
-
-            if (letterCount < 4)
-                return "영문자를 4자리 이상 입력해주세요.";
-
-            if (_dbContext.UserInfos.Any(u => u.Id == userId))
-                return "이미 존재하는 아이디입니다.";
-
-            return string.Empty;
+            return inputIdResult;
         }
 
         /// <summary>
@@ -109,17 +147,26 @@ namespace LoginApp.Utils
         private static string CheckPassword(string password)
         {
             int letterCount = password.Count(char.IsLetter);
+            string inputPwdResult = string.Empty;
 
             if (string.IsNullOrEmpty(password))
-                return "비밀번호를 입력해주세요.";
+            {
+                inputPwdResult = "비밀번호를 입력해주세요.";
+            }
+            else if (letterCount < 6)
+            {
+                inputPwdResult = "영문자를 6자리 이상 입력해주세요.";
+            }
+            else if (!PasswordRegex().IsMatch(password))
+            {
+                inputPwdResult = "숫자, 특수문자(!,@,#,$,%)를 포함해야 합니다.";
+            }
+            else
+            {
+                return inputPwdResult;
+            }
 
-            if (letterCount < 6)
-                return "영문자를 6자리 이상 입력해주세요.";
-
-            if (!PasswordRegex().IsMatch(password))
-                return "숫자, 특수문자(!,@,#,$,%)를 포함해야 합니다.";
-
-            return string.Empty;
+            return inputPwdResult;
         }
 
         /// <summary>
@@ -130,15 +177,22 @@ namespace LoginApp.Utils
         /// <returns>조건에 따른 문자열(오류메시지) 반환. 아니면 빈문자열 반환</returns>
         private static string DoubleCheckPassword(string password, string checkPassword)
         {
+            string inputPwdCheckResult = string.Empty;
+
             if (string.IsNullOrEmpty(checkPassword))
             {
-                return "비밀번호 확인이 필요합니다";
+                inputPwdCheckResult = "비밀번호 확인이 필요합니다";
             }
-            if (password != checkPassword)
+            else if (password != checkPassword)
             {
-                return "비밀번호가 일치하지 않습니다";
+                inputPwdCheckResult = "비밀번호가 일치하지 않습니다";
             }
-            return string.Empty;
+            else
+            {
+                return inputPwdCheckResult;
+            }
+
+            return inputPwdCheckResult;
         }
 
         /// <summary>
@@ -155,9 +209,9 @@ namespace LoginApp.Utils
             string pwdCheckStatus = DoubleCheckPassword(password, checkPassword);
 
             // 아이디, 비밀번호, 비밀번호 확인 메시지가 비었을 때
-            bool isValid = string.IsNullOrEmpty(idStatus) && string.IsNullOrEmpty(pwdStatus) && string.IsNullOrEmpty(pwdCheckStatus);
+            bool isCorrect = string.IsNullOrEmpty(idStatus) && string.IsNullOrEmpty(pwdStatus) && string.IsNullOrEmpty(pwdCheckStatus);
 
-            return new SignUpAccountResult(isValid, idStatus, pwdStatus, pwdCheckStatus);
+            return isCorrect ? SignUpAccountSuccess : new(false, idStatus, pwdStatus, pwdCheckStatus);
         }
         #endregion
 
@@ -177,17 +231,28 @@ namespace LoginApp.Utils
         }
 
         /// <summary>
+        /// 회원가입(개인정보) 성공 시 반환되는 결과
+        /// </summary>
+        public static readonly SignUpPersonalResult SignUpPersonalSuccess = new(true, string.Empty, string.Empty);
+
+        /// <summary>
         /// 이름(닉네임) 입력 상태 확인 메서드
         /// </summary>
         /// <param name="name">사용자 입력 이름</param>
         /// <returns>조건에 따른 문자열(오류메시지) 반환. 아니면 빈문자열 반환</returns>
         private static string CheckName(string name)
         {
+            string inputNameResult = string.Empty;
+
             if (string.IsNullOrEmpty(name))
             {
-                return "이름을 입력해 주세요";
+                inputNameResult = "이름을 입력해 주세요";
             }
-            return string.Empty;
+            else
+            {
+                return inputNameResult;
+            }
+            return inputNameResult;
         }
 
         /// <summary>
@@ -197,15 +262,21 @@ namespace LoginApp.Utils
         /// <returns>조건에 따른 문자열(오류메시지) 반환. 아니면 빈문자열 반환</returns>
         private static string CheckBirth(string birth)
         {
+            string inputBirthResult = string.Empty;
+
             if (string.IsNullOrEmpty(birth))
             {
-                return "생년월일을 입력해 주세요";
+                inputBirthResult = "생년월일을 입력해 주세요";
             }
             else if (birth.Length != 8 || !DateOnly.TryParseExact(birth, "yyyyMMdd", null, System.Globalization.DateTimeStyles.None, out _))
             {
-                return "생년월일 형식에 맞는\n8자리 숫자로 입력해 주세요";
+                inputBirthResult = "생년월일 형식에 맞는\n8자리 숫자로 입력해 주세요";
             }
-            return string.Empty;
+            else
+            {
+                return inputBirthResult;
+            }
+            return inputBirthResult;
         }
 
         /// <summary>
@@ -219,9 +290,9 @@ namespace LoginApp.Utils
             string nameStatus = CheckName(name);
             string birthStatus = CheckBirth(birth);
 
-            bool isValid = string.IsNullOrEmpty(nameStatus) && string.IsNullOrEmpty(birthStatus);
+            bool isCorrect = string.IsNullOrEmpty(nameStatus) && string.IsNullOrEmpty(birthStatus);
 
-            return new SignUpPersonalResult(isValid, nameStatus, birthStatus);
+            return isCorrect ? SignUpPersonalSuccess : new(false, nameStatus, birthStatus);
         }
         #endregion
     }
